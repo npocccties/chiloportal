b97654d89029672b75d1f8bbaef017c13f392ea5
m-takay <m-takayanagi@intelligent-design.co.jp>
2022-08-24 11:38:16 +0900

    SQLの項目名（名前と桁数）を見直して修正

--- backend/sql/create_table.sql
+++ backend/sql/create_table.sql
@@ -1,42 +1,48 @@
-drop table if exists zxc;
-drop table if exists badges;
+/*
+drop table if exists badges cascade;
+drop table if exists categorized_badge cascade;
+drop table if exists badge_relation cascade;
+drop table if exists training cascade;
+drop table if exists consumer cascade;
+drop table if exists category cascade;
+drop table if exists course cascade;
+drop table if exists course_level cascade;
+*/
 create table badges (
 	id		serial		not null,	-- バッジID
-	badge_class	int		not null,	-- BadgeClass ID
-	badge_name	varchar(128)	not null,	-- バッジ名称
-	description	varchar(512)	null,		-- 説明
-	image_id	varchar(256)	null,		-- 画像 ID
-	image_author	varchar(256)	null,		-- 画像 author
-	ob_version	varchar(256)	null,		-- OBバージョン
+	badge_class_id	text		not null,	-- BadgeClass ID
+	name		varchar(512)	not null,	-- バッジ名称
+	description	text		null,		-- 説明
+	narrative	text		null,		-- 基準
+	image_id	text		null,		-- 画像 ID
+	image_author	text		null,		-- 画像 author
+	ob_version	text		null,		-- OBバージョン
 	issuer_name	varchar(128)	null,		-- 発行者
-	badge_type	char(9)		not null,	-- バッジ種別(wisdom or knowledge)
-	badge_url	varchar(256)	null,		-- URL
+	issuer_id	text		null,		-- issuer id
+	type		char(9)		not null,	-- バッジ種別(wisdom or knowledge)
+	url		text		null,		-- URL
 	primary key (id)
 ); -- 'バッジ'
-drop table if exists badge_relation;
 create table badge_relation (
 	id			serial		not null,	-- Relation ID
 	wisdom_badge_id		int		not null	REFERENCES badges,	-- Wisdom Badge ID
 	knowledge_badge_id	int		not null	REFERENCES badges,	-- Knowlwdge Badge ID
 	primary key (id)
 ); -- '関連バッジ'
-drop table if exists training;
 create table training (
 	id		serial		not null,	-- 研修内容 ID
 	badge_id	int		not null	REFERENCES badges,	-- Badge ID
-	training_type	varchar(32)	not null,	-- 種類
-	training_name	varchar(256)	not null,	-- 名称
+	type		varchar(32)	not null,	-- 種類
+	name		varchar(256)	not null,	-- 名称
 	primary key (id)
 ); -- '研修内容'
-drop table if exists consumer;
 create table consumer (
-	id			serial		not null,	-- コンシューマ ID
-	consumer_name		varchar(256)	not null,	-- 名称
-	consumer_url		varchar(256)	null,		-- URL
-	consumer_mailaddr	varchar(256)	null,		-- 連絡先メールアドレス
+	id		serial		not null,	-- コンシューマ ID
+	name		varchar(256)	not null,	-- 名称
+	url		text		null,		-- URL
+	mailaddr	varchar(256)	null,		-- 連絡先メールアドレス
 	primary key (id)
 ); -- 'コンシューマ'
-drop table if exists category;
 create table category (
 	id		serial		not null,	-- カテゴリID
 	consumer_id	int		not null	REFERENCES consumer,	-- コンシューマ ID
@@ -45,23 +51,20 @@ create table category (
 	category3_name	varchar(128)	not null,	-- カテゴリ3
 	primary key (id)
 ); -- 'カテゴリ'
-drop table if exists course;
 create table course (
 	id		serial		not null,	-- 受講対象ID
 	consumer_id	int		not null	REFERENCES consumer,	-- コンシューマ ID
-	course_name	varchar(256)	not null,	-- 名称
-	description	varchar(512)	not null,	-- 説明
+	name		varchar(256)	not null,	-- 名称
+	description	text		not null,	-- 説明
 	primary key (id)
 ); -- '受講対象'
-drop table if exists course_level;
 create table course_level (
 	id		serial		not null,	-- 受講レベルID
 	consumer_id	int		not null	REFERENCES consumer,	-- コンシューマ ID
-	level_name	varchar(256)	not null,	-- 名称
-	description	varchar(512)	not null,	-- 説明
+	name		varchar(256)	not null,	-- 名称
+	description	text		not null,	-- 説明
 	primary key (id)
 ); -- '受講レベル'
-drop table if exists categorized_badge;
 create table categorized_badge (
 	id		serial		not null,	-- カテゴライズID
 	consumer_id	int		not null	REFERENCES consumer,		-- コンシューマ ID
@@ -69,6 +72,6 @@ create table categorized_badge (
 	badge_id	int		not null	REFERENCES badges,		-- Badge ID
 	course_id	int		not null	REFERENCES course,		-- 受講対象 ID
 	course_level_id	int		not null	REFERENCES course_level,	-- 受講レベル ID
-	description	varchar(512)	not null,	-- 説明
+	description	text		not null,	-- 説明
 	primary key (id)
 ); -- 'カテゴライズドバッジ'
