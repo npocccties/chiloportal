c374c8c39208d62bd13b9b50da7c33e3fec8fcbf
m-takay <m-takayanagi@intelligent-design.co.jp>
2022-09-12 18:53:38 +0900

    項目名の修正、ほか

--- backend/sql/create_table.sql
+++ backend/sql/create_table.sql
@@ -1,8 +1,10 @@
+drop table if exists course cascade;
+
 drop table if exists portal_category cascade;
 drop table if exists issuer cascade;
 drop table if exists wisdom_badges cascade;
 drop table if exists knowledge_badges cascade;
-drop table if exists course cascade;
+drop table if exists criteria cascade;
 drop table if exists consumer cascade;
 drop table if exists target_occupations cascade;
 drop table if exists category cascade;
@@ -13,13 +15,14 @@ drop table if exists categorised_badges cascade;
 create table portal_category (
 	id		serial		not null,	-- カテゴリ ID
 	name		varchar(128)	not null,	-- カテゴリ
+	description	text		null,		-- 説明
 	sort_key	int		not null,	-- 表示順
 	primary key (id)
 ); -- 'ポータル独自カテゴリ'
 create table issuer (
 	id		serial		not null,	-- 発行者ID
 	name		varchar(256)	null,		-- 発行者名
-	url		text		null,		-- url
+	url		text		null,		-- URL
 	email		text		null,		-- email
 	primary key (id)
 ); -- '発行者
@@ -29,12 +32,12 @@ create table wisdom_badges (
 	badge_class_id	text		not null,	-- BadgeClass ID
 	name		varchar(512)	not null,	-- バッジ名称
 	description	text		null,		-- 説明
-	narrative	text		null,		-- 基準
+	criteria_narrative	text	null,		-- 基準
 	image_id	text		null,		-- 画像 ID
 	image_author	text		null,		-- 画像 author
-	ob_version	text		null,		-- OBバージョン
+	version		text		null,		-- OBバージョン
 	issuer_id	int		null		REFERENCES issuer,	-- 発行者のID
-	url		text		null,		-- URL
+	alignment_targeturl	text	null,		-- URL
 	primary key (id)
 ); -- '能力バッジ'
 create table knowledge_badges (
@@ -43,14 +46,14 @@ create table knowledge_badges (
 	badge_class_id	text		not null,	-- BadgeClass ID
 	name		varchar(512)	not null,	-- バッジ名称
 	description	text		null,		-- 説明
-	narrative	text		null,		-- 基準
+	criteria_narrative	text	null,		-- 基準
 	image_id	text		null,		-- 画像 ID
 	image_author	text		null,		-- 画像 author
-	ob_version	text		null,		-- OBバージョン
+	version		text		null,		-- OBバージョン
 	issuer_id	int		null		REFERENCES issuer,	-- 発行者のID
 	primary key (id)
 ); -- '知識バッジ'
-create table course (
+create table criteria (
 	id		serial		not null,	-- 研修内容 ID
 	knowledge_badges_id	int	not null	REFERENCES knowledge_badges,	-- 知識バッジのID
 	type		varchar(32)	not null,	-- 種類
@@ -96,7 +99,7 @@ create table cell (
 	id			serial	not null,	-- セルID
 	target_occupations_id	int	not null	REFERENCES target_occupations,	-- 受講対象 ID
 	category_id		int	not null	REFERENCES category,	-- カテゴリ ID
-	carrier_stage_id	int	not null	REFERENCES target_carrier_stage,	-- 受講レベル ID
+	target_carrier_stage_id	int	not null	REFERENCES target_carrier_stage,	-- 受講レベル ID
 	description		text	not null,	-- 説明
 	primary key (id)
 ); -- 'セル'
