e9c0fe98e64cee5f270ee0ac97e0eb1c42b491f8
m-takay <m-takayanagi@intelligent-design.co.jp>
2022-09-22 11:02:15 +0900

    用語整理にともなうテーブル名と項目名の修正

--- backend/sql/create_table.sql
+++ backend/sql/create_table.sql
@@ -3,15 +3,16 @@ drop table if exists issuer cascade;
 drop table if exists wisdom_badges cascade;
 drop table if exists knowledge_badges cascade;
 drop table if exists criteria cascade;
+drop table if exists categorised_badges cascade;
 drop table if exists consumer cascade;
-drop table if exists training_index cascade;
-drop table if exists category cascade;
+drop table if exists framework cascade;
+drop table if exists competency cascade;
 drop table if exists stage cascade;
-drop table if exists compitency cascade;
+drop table if exists goal cascade;
 -- ----------------------------------------------------------------------------------------------------
 create table portal_category (
-	id		serial		not null,	-- カテゴリ ID
-	name		varchar(128)	not null,	-- カテゴリ
+	id		serial		not null,	-- ポータルカテゴリID
+	name		varchar(128)	not null,	-- ポータルカテゴリ名
 	description	text		null,		-- 説明
 	sort_key	int		not null,	-- 表示順
 	primary key (id)
@@ -25,10 +26,11 @@ create table issuer (
 ); -- '発行者
 create table wisdom_badges (
 	id		serial		not null,	-- バッジID
-	portal_category_id	int	null		REFERENCES portal_category,	-- ポータル独自カテゴリのID
+	portal_category_id	int	null		REFERENCES portal_category,	-- ポータルカテゴリID
 	badge_class_id	text		not null,	-- BadgeClass ID
 	name		varchar(512)	not null,	-- バッジ名称
 	description	text		null,		-- 説明
+	tags		text		null,		-- 自由キーワード
 	image_id	text		null,		-- 画像 ID
 	image_author	text		null,		-- 画像 author
 	version		text		null,		-- OBバージョン
@@ -43,6 +45,7 @@ create table knowledge_badges (
 	badge_class_id	text		not null,	-- BadgeClass ID
 	name		varchar(512)	not null,	-- バッジ名称
 	description	text		null,		-- 説明
+	tags		text		null,		-- 自由キーワード
 	criteria_narrative	text	null,		-- 基準
 	image_id	text		null,		-- 画像 ID
 	image_author	text		null,		-- 画像 author
@@ -51,7 +54,7 @@ create table knowledge_badges (
 	primary key (id)
 ); -- '知識バッジ'
 create table criteria (
-	id		serial		not null,	-- 研修内容 ID
+	id		serial		not null,	-- 研修内容ID
 	knowledge_badges_id	int	not null	REFERENCES knowledge_badges,	-- 知識バッジのID
 	type		varchar(32)	not null,	-- 種類
 	name		varchar(256)	not null,	-- 名称
@@ -60,52 +63,54 @@ create table criteria (
 ); -- '研修内容'
 -- ----------------------------------------------------------------------------------------------------
 create table consumer (
-	id		serial		not null,	-- コンシューマ ID
+	id		serial		not null,	-- コンシューマID
 	name		varchar(256)	not null,	-- 名称
 	url		text		null,		-- URL
 	email		varchar(256)	null,		-- 連絡先メールアドレス
 	primary key (id)
 ); -- 'コンシューマ'
-create table training_index (
-	id		serial		not null,	-- 受講対象ID
-	consumer_id	int		not null	REFERENCES consumer,	-- コンシューマ ID
+-- ----------------------------------------------------------------------------------------------------
+create table framework (
+	id		serial		not null,	-- 教員育成指標ID
+	consumer_id	int		not null	REFERENCES consumer,	-- コンシューマID
 	name		varchar(256)	not null,	-- 名称
 	description	text		not null,	-- 説明
 	supplementary	text		not null,	-- 補足説明
+	url		text		not null,	-- PDFのURL
 	sort_key	int		not null,	-- 表示順
 	primary key (id)
-); -- '受講対象'
+); -- '教員育成指標'
 -- ----------------------------------------------------------------------------------------------------
-create table category (
-	id		serial		not null,	-- カテゴリ ID
-	category1_name	varchar(128)	not null,	-- カテゴリ1
-	category2_name	varchar(128)	not null,	-- カテゴリ2
-	category3_name	varchar(128)	not null,	-- カテゴリ3
-	sort_key	int		not null,	-- 表示順
-	primary key (id)
-); -- 'カテゴリ'
 create table stage (
-	id		serial		not null,	-- 受講レベルID
+	id		serial		not null,	-- 成長段階ID
 	name		varchar(256)	not null,	-- 名称
 	sub_name	text		not null,	-- 自由記述欄
 	description	text		not null,	-- 説明
 	sort_key	int		not null,	-- 表示順
 	primary key (id)
-); -- '受講レベル'
+); -- '成長段階'
+create table competency (
+	id		serial		not null,	-- 指標項目ID
+	group1_name	varchar(128)	not null,	-- 指標項目グループ1
+	group2_name	varchar(128)	not null,	-- 指標項目グループ2
+	group3_name	varchar(128)	not null,	-- 指標項目グループ3
+	sort_key	int		not null,	-- 表示順
+	primary key (id)
+); -- '指標項目'
 -- ----------------------------------------------------------------------------------------------------
-create table compitency (
-	id			serial	not null,	-- セルID
-	training_index_id	int	not null	REFERENCES training_index,	-- 受講対象 ID
-	category_id		int	not null	REFERENCES category,	-- カテゴリ ID
-	stage_id		int	not null	REFERENCES stage,	-- 受講レベル ID
+create table goal (
+	id			serial	not null,	-- 目標ID
+	framework_id		int	not null	REFERENCES framework,	-- 教員育成指標ID
+	competency_id		int	not null	REFERENCES competency,	-- 指標項目ID
+	stage_id		int	not null	REFERENCES stage,	-- 成長段階ID
 	description		text	not null,	-- 説明
 	primary key (id)
-); -- 'セル'
+); -- '指標項目の成長段階毎の目標'
 -- ----------------------------------------------------------------------------------------------------
 create table categorised_badges (
 	id			serial	not null,	-- カテゴライズID
 	wisdom_badges_id	int	not null	REFERENCES wisdom_badges,	-- 能力バッジのID
-	compitency_id		int	not null	REFERENCES compitency,	-- セル ID
+	goal_id			int	not null	REFERENCES goal,	-- 目標ID
 	description		text	not null,	-- 説明
 	primary key (id)
 ); -- 'カテゴライズドバッジ'
