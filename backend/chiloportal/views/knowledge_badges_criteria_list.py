c5ad60a1be630f6c3b17b0203209ace97bf0f683
yoshinobu-etou-id <yoshinobu_eto@intelligent-design.co.jp>
2022-09-26 17:43:19 +0900

    バッジの研修内容一覧取得API実装

---
+++ backend/chiloportal/views/knowledge_badges_criteria_list.py
@@ -0,0 +1,22 @@
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+from ..models import *
+from ..swagger import *
+from ..responses import *
+from .base_api_view import *
+from .. import utils
+
+class KnowledgeBadgesCriteriaList(BaseAPIView):
+    swagger_query_params = [SwaggerQueryParam('badges_id', True)]
+    filter_backends = (SwaggerQueryParamFilter,)
+    def _get(self, request):
+        id = self.request.GET.get('badges_id')
+        if id == None or utils.is_int(id) == False:
+            raise ParseError('Invalid ID supplied')
+        queryset = Criteria.objects.filter(knowledge_badges = id).order_by('sort_key')
+        if queryset.exists() == False:
+            raise NotFound('Criteria not found')
+        result = []
+        for criteria in queryset:
+            result.append(to_criteria(criteria))
+        return Response(result)
