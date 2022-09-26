bdb7721f2efeabf3f9d94ef793616147a7d32775
yoshinobu-etou-id <yoshinobu_eto@intelligent-design.co.jp>
2022-09-26 17:47:08 +0900

    育成指標に関連する指標項目一覧API実装

---
+++ backend/chiloportal/views/framework_field_list.py
@@ -0,0 +1,19 @@
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound,ParseError
+from ..models import *
+from .. import utils
+from ..swagger import *
+from ..responses import *
+from .base_api_view import *
+
+class FrameworkFieldList(BaseAPIView):
+    swagger_query_params = [SwaggerQueryParam('framework_id', True)]
+    filter_backends = (SwaggerQueryParamFilter,)
+    def _get(self, request):
+        id = self.request.GET.get('framework_id')
+        if id == None or utils.is_int(id) == False:
+            raise ParseError('Invalid ID supplied')
+        queryset = Field.objects.filter(goal_field__framework_id = id)
+        if queryset.exists() == False:
+            raise NotFound('Stage not found')
+        return Response(to_fields(queryset))
