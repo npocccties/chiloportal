ebe6bca3a9774c5faa85ef05d302f59d5adf9caf
yoshinobu-etou-id <yoshinobu_eto@intelligent-design.co.jp>
2022-09-26 17:50:21 +0900

    成長段階に関連する指標項目一覧API実装

--- backend/chiloportal/views/__init__.py
+++ backend/chiloportal/views/__init__.py
@@ -1,6 +1,7 @@
 from .consumer_detail import *
 from .consumer_list import *
 from .framework_field_list import *
+from .stage_field_list import *
 from .portal_category_list import *
 from .badges_detail import *
 from .wisdom_badges_list import *
---
+++ backend/chiloportal/views/stage_field_list.py
@@ -0,0 +1,19 @@
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound,ParseError
+from ..models import *
+from .. import utils
+from ..swagger import *
+from ..responses import *
+from .base_api_view import *
+
+class StageFieldList(BaseAPIView):
+    swagger_query_params = [SwaggerQueryParam('stage_id', True)]
+    filter_backends = (SwaggerQueryParamFilter,)
+    def _get(self, request):
+        id = self.request.GET.get('stage_id')
+        if id == None or utils.is_int(id) == False:
+            raise ParseError('Invalid ID supplied')
+        queryset = CategorisedBadges.objects.filter(goal__stage_id = id).select_related('goal__field', 'wisdom_badges').order_by('pk').distinct()
+        if queryset.exists() == False:
+            raise NotFound('Field not found')
+        return Response(to_fields_detail(queryset))
