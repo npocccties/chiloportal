6127003857fb9353c063464a862f85d9ec761f76
yoshinobu-etou-id <yoshinobu_eto@intelligent-design.co.jp>
2022-09-28 09:44:59 +0900

    指定したコンシューマIDの育成指標一覧API実装

---
+++ backend/chiloportal/views/consumer_framework_list.py
@@ -0,0 +1,19 @@
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+from ..models import *
+from ..swagger import *
+from ..responses import *
+from .base_api_view import *
+from .. import utils
+
+class ConsumerFrameworkList(BaseAPIView):
+    swagger_query_params = [SwaggerQueryParam('consumer_id', True)]
+    filter_backends = (SwaggerQueryParamFilter,)
+    def _get(self, request):
+        id = request.GET.get('consumer_id')
+        if id == None or utils.is_int(id) == False:
+            raise ParseError('Invalid ID supplied')
+        queryset = Framework.objects.filter(consumer_id = id).order_by('sort_key')
+        if queryset.exists() == False:
+            raise NotFound('Framework not found')
+        return Response(to_frameworks(queryset))
