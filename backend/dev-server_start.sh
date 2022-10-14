94d479957b33ef227e6ba733aab596cc5548377f
yoshinobu-etou-id <yoshinobu_eto@intelligent-design.co.jp>
2022-10-14 17:56:15 +0900

    chore: 便利スクリプトに漏れがあったので追記

--- backend/dev-server_start.sh
+++ backend/dev-server_start.sh
@@ -3,4 +3,5 @@ sudo cp .env.dev-server .env
 sudo cp docker-compose.dev-server.yml docker-compose.yml
 docker-compose up -d
 docker-compose exec app python /workspace/manage.py makemigrations
+docker-compose exec app python /workspace/manage.py migrate
 docker-compose exec -d app python /workspace/manage.py collectstatic --no-input --clear
