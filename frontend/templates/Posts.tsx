64e605c9b16a9159855e7348e8485e008f10ed10
Kimiaki Kuno <knokmki612@gmail.com>
2022-10-17 18:07:31 +0900

    feat: ぱんくずリストの実装

--- frontend/templates/Posts.tsx
+++ frontend/templates/Posts.tsx
@@ -1,10 +1,16 @@
 import Link from "next/link";
 import { Props } from "pages/posts";
+import Breadcrumbs from "components/Breadcrumbs";
 import { pagesPath } from "lib/$path";
 
 function Posts({ posts }: Props) {
   return (
     <article className="max-w-4xl mx-auto my-16 px-4 min-h-[33vh]">
+      <Breadcrumbs
+        className="mb-6"
+        nodes={[{ name: "トップ", href: pagesPath.$url() }]}
+        leaf="OKUTEPからのおしらせ"
+      />
       <h1 className="text-2xl text-gray-700 mb-4">OKUTEPからのおしらせ</h1>
       <ul className="list-disc pl-8 text-gray-700">
         {posts.map((post) => (
