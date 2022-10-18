71a69cb547a076dfa6d5fef9a6df2b832ff1b648
Kimiaki Kuno <knokmki612@gmail.com>
2022-10-18 19:10:20 +0900

    feat: ポータルカテゴリトップ画面の実装

---
+++ frontend/components/WisdomBadgesItem.tsx
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import Image from "next/future/image";
+import { BadgeDetail2 } from "api/@types";
+import { pagesPath } from "lib/$path";
+
+type Props = {
+  wisdomBadges: BadgeDetail2;
+};
+
+function WisdomBadgesItem({ wisdomBadges }: Props) {
+  return (
+    <Link
+      href={pagesPath.wisdom_badges
+        ._wisdomBadgesId(wisdomBadges.badges_id)
+        .$url()}
+    >
+      <a className="flex gap-4">
+        <div className="flex-shrink-0">
+          <Image
+            src={`/images/${wisdomBadges.image}`}
+            width={80}
+            height={88}
+            alt=""
+          />
+          <div className="jumpu-tag">能力バッジ</div>
+        </div>
+        <div>
+          <h3 className="text-lg">{wisdomBadges.name}</h3>
+          <p className="text-sm mb-2">
+            <span className="font-bold mr-1">{wisdomBadges.issuer_name}</span>
+            <span className="text-gray-500">発行</span>
+          </p>
+          <p className="text-sm text-gray-600 mb-2 line-clamp-2">
+            {wisdomBadges.description}
+          </p>
+          {"knowledge_badges_list" in wisdomBadges.detail && (
+            <div className="jumpu-tag">
+              知識バッジ{wisdomBadges.detail.knowledge_badges_list.length}
+              つで獲得
+            </div>
+          )}
+        </div>
+      </a>
+    </Link>
+  );
+}
+
+export default WisdomBadgesItem;
--- frontend/templates/PortalCategory.tsx
+++ frontend/templates/PortalCategory.tsx
@@ -1,8 +1,46 @@
+import Image from "next/future/image";
+import Breadcrumbs from "components/Breadcrumbs";
+import Container from "components/Container";
+import WisdomBadgesItem from "components/WisdomBadgesItem";
 import { Props } from "pages/portal_categories/[portalCategoryId]";
+import { pagesPath } from "lib/$path";
 
 export default function PortalCategory({
   portalCategory,
   wisdomBadgesList,
 }: Props) {
-  return null;
+  return (
+    <Container>
+      <Breadcrumbs
+        className="mb-6"
+        nodes={[{ name: "トップ", href: pagesPath.$url() }]}
+        leaf={portalCategory.name}
+      />
+      <header className="flex gap-8 items-center mb-16">
+        <Image
+          className="rounded-2xl"
+          src={`/images/${portalCategory.image_url_path}`}
+          width={200}
+          height={200}
+          alt=""
+        />
+        <div>
+          <h1 className="text-4xl text-gray-600 mb-2">{portalCategory.name}</h1>
+          <p className="font-bold text-gray-600 mb-4">
+            {portalCategory.badges_count}つの能力バッジがあります
+          </p>
+          <p className="text-gray-500">{portalCategory.description}</p>
+        </div>
+      </header>
+      <article>
+        <ul>
+          {wisdomBadgesList.badges.map((wisdomBadges) => (
+            <li className="mb-8" key={wisdomBadges.badges_id}>
+              <WisdomBadgesItem wisdomBadges={wisdomBadges} />
+            </li>
+          ))}
+        </ul>
+      </article>
+    </Container>
+  );
 }
