9dc7420dd6b7459c9f1979ca7b24c42cf56bbd8c
Kimiaki Kuno <knokmki612@gmail.com>
2022-10-20 19:01:04 +0900

    feat: 能力バッジを認定している教育委員会別育成指標ダイアログの実装

---
+++ frontend/components/WisdomBadgesDialog.tsx
@@ -0,0 +1,111 @@
+import { useState } from "react";
+import { Fragment } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { BadgeDetail2 } from "api/@types";
+import useBadgesFrameworks from "lib/use-badges-frameworks";
+
+function BadgesFrameworks({
+  consumers,
+  frameworksPerConsumers,
+}: Exclude<ReturnType<typeof useBadgesFrameworks>["data"], undefined>) {
+  return (
+    <>
+      {consumers.map((consumer, consumerIndex) => {
+        const frameworks = frameworksPerConsumers[consumerIndex];
+        return (
+          <section key={consumer.consumer_id} className="mb-4">
+            <h3 className="font-bold mb-1">{consumer.name}</h3>
+            <ul className="pl-4">
+              {frameworks.map((framework, frameworkIndex) => (
+                <li key={framework.framework_id} className="inline">
+                  {/* TODO: 育成指標への動線を用意して */}
+                  <span className="mr-1">{framework.name}</span>
+                  {frameworks.length > frameworkIndex + 1 && (
+                    <span className="mr-1" aria-hidden>
+                      ／
+                    </span>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </section>
+        );
+      })}
+    </>
+  );
+}
+
+type Props = {
+  wisdomBadges: BadgeDetail2;
+  open: boolean;
+  onClose(): void;
+};
+
+function WisdomBadgesDialog({ wisdomBadges, open, onClose }: Props) {
+  const [shouldFetch, setShouldFetch] = useState(false);
+  const { data: badgesFrameworks } = useBadgesFrameworks(
+    wisdomBadges.badges_id,
+    shouldFetch
+  );
+  const handleEnter = () => {
+    setShouldFetch(true);
+  };
+  const handleLeave = () => {
+    setShouldFetch(false);
+  };
+  return (
+    <Transition appear show={open} as={Fragment}>
+      <Dialog as="div" className="relative z-20" onClose={onClose}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+          beforeEnter={handleEnter}
+          afterLeave={handleLeave}
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="jumpu-card w-full max-w-xl rounded-xl bg-white p-6 shadow-xl">
+                <Dialog.Title className="text-lg font-bold text-gray-500 mb-6">
+                  このバッジは以下の教育委員会に認定されています
+                </Dialog.Title>
+                {badgesFrameworks ? (
+                  <BadgesFrameworks {...badgesFrameworks} />
+                ) : (
+                  <div
+                    className="flex justify-center items-center w-full h-48"
+                    aria-hidden
+                  >
+                    <div className="jumpu-spinner">
+                      <svg viewBox="24 24 48 48">
+                        <circle cx="48" cy="48" r="16" />
+                      </svg>
+                    </div>
+                  </div>
+                )}
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+}
+
+export default WisdomBadgesDialog;
