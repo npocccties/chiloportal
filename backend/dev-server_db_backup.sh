14b6668a69a09d6f35dcda04edfd6913bbe91a05
yoshinobu-etou-id <yoshinobu_eto@intelligent-design.co.jp>
2022-10-22 06:19:48 +0900

    chore: 別ユーザーでコンテナ再起動するとCORSのモジュールが見つからないエラーが発生するので、コンテナ再起動時はコンテナを作り直す

--- backend/dev-server_db_backup.sh
+++ backend/dev-server_db_backup.sh
@@ -2,9 +2,7 @@
 DIR=`dirname "${0}"`
 cd $DIR
 docker-compose exec db pg_dump -h 127.0.0.1 \
-    -p 5432 -d develop -U postgres -t portal_category -t issuer \
-    -t wisdom_badges -t knowledge_badges -t criteria -t categorised_badges \
-    -t consumer -t framework -t field -t stage -t goal \
+    -p 5432 -d develop -U postgres \
     -Fc -v --file=/var/lib/postgresql/chiloportal.dump
 BACKUP_FILE="/opt/chiloportal/backend/postgresql/data/chiloportal.dump"
 if [ -e $BACKUP_FILE ]; then
