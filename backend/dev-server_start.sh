d6152a0c32876d5ccf9b80979f996eb5bec49814
yoshinobu-etou-id <yoshinobu_eto@intelligent-design.co.jp>
2022-10-24 13:38:14 +0900

    chore: GitHub Actionsからデプロイするときに「the input device is not a TTY」と出力されるので、TTY割り当てを無効化するオプションを追加

--- backend/dev-server_start.sh
+++ backend/dev-server_start.sh
@@ -7,9 +7,9 @@ docker-compose up -d
 /bin/sh ./dev-server_db_restore.sh
 # コンテナ作成時に requirements.txt から django-cors-headers をインストールしても /usr/local/lib/python3.9/site-packages にインストールされていないので、
 # デプロイのタイミングで django-cors-headers をインストールする
-docker-compose exec app pip install --upgrade pip
-docker-compose exec app pip install django-cors-headers
-docker-compose exec app python /workspace/manage.py makemigrations
-docker-compose exec app python /workspace/manage.py migrate
+docker-compose exec -T app pip install --upgrade pip
+docker-compose exec -T app pip install django-cors-headers
+docker-compose exec -T app python /workspace/manage.py makemigrations
+docker-compose exec -T app python /workspace/manage.py migrate
 docker-compose exec -d app python /workspace/manage.py collectstatic --no-input --clear
 docker-compose exec -d app gunicorn project.wsgi:application --bind 0.0.0.0:8000
