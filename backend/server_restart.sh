4391db3f5e860be4c795a4cd5c5ec40286861a51
yoshinobu-etou-id <yoshinobu_eto@intelligent-design.co.jp>
2022-10-25 20:09:42 +0900

    chore: シェルスクリプトの変数定義位置修正

--- backend/server_restart.sh
+++ backend/server_restart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-/bin/sh server_stop.sh $ENV_FILE
 readonly ENV_FILE=$1
+/bin/sh server_stop.sh $ENV_FILE
 readonly DOCKER_COMPOSE_YML=$2
 /bin/sh server_start.sh $ENV_FILE $DOCKER_COMPOSE_YML
