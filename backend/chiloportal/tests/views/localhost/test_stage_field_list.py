0f825b52fb70ef6cdc37545d8b28c975ae818329
yoshinobu-etou-id <yoshinobu_eto@intelligent-design.co.jp>
2022-10-26 20:00:56 +0900

    fix: field1に連想配列を入れていたが、配列を入れるのが正しいので修正した

--- backend/chiloportal/tests/views/localhost/test_stage_field_list.py
+++ backend/chiloportal/tests/views/localhost/test_stage_field_list.py
@@ -11,29 +11,30 @@ class StageFieldListTests(BaseAPIViewTests):
         response = self.request_normal(
             factory, view, self.stage_field_list_url, {"stage_id": self.stg1.id}
         )
-        self.assertEqual(len(response.data), 5)
-        array = response.data
-        field1 = array[0].get("field1")
+        data = response.data
+        array = data.get("field1")
+        self.assertEqual(len(array), 5)
+        field1 = array[0]
         fields2 = field1.get("field2")
         fields3 = fields2[0].get("field3")
         self.assert_field(field1, fields2[0], fields3[0], self.field1, [self.wb1.id])
         self.assert_field(field1, fields2[0], fields3[1], self.field2, [self.wb1.id])
-        field1 = array[1].get("field1")
+        field1 = array[1]
         fields2 = field1.get("field2")
         fields3 = fields2[0].get("field3")
         self.assert_field(field1, fields2[0], fields3[0], self.field3, [self.wb2.id])
         fields3 = fields2[1].get("field3")
         self.assert_field(field1, fields2[1], fields3[0], self.field4, [self.wb2.id])
-        field1 = array[2].get("field1")
+        field1 = array[2]
         fields2 = field1.get("field2")
         fields3 = fields2[0].get("field3")
         self.assert_field(field1, fields2[0], fields3[0], self.field5, [self.wb3.id])
         self.assert_field(field1, fields2[0], fields3[1], self.field6, [self.wb3.id])
-        field1 = array[3].get("field1")
+        field1 = array[3]
         fields2 = field1.get("field2")
         fields3 = fields2[0].get("field3")
         self.assert_field(field1, fields2[0], fields3[0], self.field7, [self.wb4.id])
-        field1 = array[4].get("field1")
+        field1 = array[4]
         fields2 = field1.get("field2")
         fields3 = fields2[0].get("field3")
         self.assert_field(field1, fields2[0], fields3[0], self.field8, [self.wb5.id])
