15ec8b698c6e486529022d21034404f18d129aa8
Kimiaki Kuno <knokmki612@gmail.com>
2022-10-28 08:16:04 +0900

    refactor: コードの整理
    - swr のキーの命名規則の統一
    - 参照する型が結果的に同じだが実体が不一致

--- frontend/lib/use-portal-categories.ts
+++ frontend/lib/use-portal-categories.ts
@@ -2,7 +2,7 @@ import useSWRImmutable from "swr/immutable";
 import { client } from "lib/client";
 import { PortalCategory } from "api/@types";
 
-const key = "portal_category/list" as const;
+const key = "portalCategory/list" as const;
 
 async function fetcher(_: typeof key) {
   return client.portalCategory.list.$get();
