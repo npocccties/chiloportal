a54b2c5f09ce5c73d47a357ecebba3f0fbf3cde2
yoshinobu-etou-id <yoshinobu_eto@intelligent-design.co.jp>
2022-10-27 15:19:32 +0900

    chore: デプロイ時に frontend のディレクトリに移動できていなかったので移動する

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -9,6 +9,8 @@ else
    sudo git clone https://github.com/npocccties/chiloportal.git
 fi
 # frontend
+readonly FRONTEND_DIR="${CHECKOUT_DIR}frontend"
+cd $FRONTEND_DIR
 docker build -t frontend .
 docker run --rm -p 3000:3000 --name frontend --net app_network --detach frontend
 # backend
