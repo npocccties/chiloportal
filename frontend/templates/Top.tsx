dd21a02aa9f4355e8041dd39a8d2c93c51709488
Kimiaki Kuno <knokmki612@gmail.com>
2022-10-28 12:57:40 +0900

    refactor!: npx @next/codemod next-image-to-legacy-image templates

--- frontend/templates/Top.tsx
+++ frontend/templates/Top.tsx
@@ -1,6 +1,6 @@
 import clsx from "clsx";
 import Link from "next/link";
-import Image from "next/future/image";
+import Image from "next/image";
 import { Props } from "pages";
 import { pagesPath } from "lib/$path";
 import useConsumers from "lib/use-consumers";
