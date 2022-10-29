fc417faed4ff26fd53ac3afe88931c7cf65b999f
yoshinobu-etou-id <yoshinobu_eto@intelligent-design.co.jp>
2022-10-29 16:26:28 +0900

    fix: DOCKER_COMPOSE_YML の定義移動

--- backend/server_restart.sh
+++ backend/server_restart.sh
@@ -4,6 +4,6 @@ DIR=$(cd $(dirname $0); pwd)
 echo $DIR
 cd $DIR
 readonly ENV_FILE=$1
-/bin/sh server_stop.sh $ENV_FILE $DOCKER_COMPOSE_YML
 readonly DOCKER_COMPOSE_YML=$2
+/bin/sh server_stop.sh $ENV_FILE $DOCKER_COMPOSE_YML
 /bin/sh server_start.sh $ENV_FILE $DOCKER_COMPOSE_YML
