c81ace601c8aeb981e92eaebd8e06f0507367c2f
yoshinobu-etou-id <yoshinobu_eto@intelligent-design.co.jp>
2022-10-29 15:48:38 +0900

    fix: ディレクトリ移動見直し

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
+DIR=$(cd $(dirname $0); pwd)
 sudo git checkout .
 sudo git pull
 sudo chmod 755 deploy.sh
 docker network rm app_network
 docker network create -d bridge app_network
 # frontend
-cd frontend
+cd "{$DIR}/frontend"
 docker build -t frontend .
 docker container stop frontend 2>&1 || true
 docker run --rm -p 3000:3000 --name frontend --net app_network --detach frontend
 # backend
-cd ../backend
+cd "{$DIR}/backend"
 sudo chmod 755 server_*.sh
 readonly ENV_FILE=$1
 readonly DOCKER_COMPOSE_YML=$2
