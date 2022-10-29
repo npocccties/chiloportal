27fa3fdff34990849a6adeba9ce01348c5d84f74
yoshinobu-etou-id <yoshinobu_eto@intelligent-design.co.jp>
2022-10-29 16:19:53 +0900

    fix: カレントディレクトリの移動を試みる

--- backend/server_db_restore.sh
+++ backend/server_db_restore.sh
@@ -2,5 +2,6 @@
 echo $0
 DIR=$(cd $(dirname $0); pwd)
 echo $DIR
+cd $DIR
 source ./.env
 docker-compose exec -T db pg_restore --clean -h 127.0.0.1 -p 5432 -d $DB_NAME -U $DB_USER -v < /var/chiloportal.dump
