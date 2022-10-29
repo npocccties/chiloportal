6f016f0722f125657264c2c9c50a777689cdac91
yoshinobu-etou-id <yoshinobu_eto@intelligent-design.co.jp>
2022-10-29 15:53:42 +0900

    fix: 再度変数参照誤り修正

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,14 @@ sudo chmod 755 deploy.sh
 docker network rm app_network
 docker network create -d bridge app_network
 # frontend
-cd "${DIR}/frontend"
+readonly FRONTEND_DIR="${CHECKOUT_DIR}frontend"
+cd $FRONTEND_DIR
 docker build -t frontend .
 docker container stop frontend 2>&1 || true
 docker run --rm -p 3000:3000 --name frontend --net app_network --detach frontend
 # backend
-cd "${DIR}/backend"
+readonly BACKEND_DIR="${CHECKOUT_DIR}backend"
+cd $BACKEND_DIR
 sudo chmod 755 server_*.sh
 readonly ENV_FILE=$1
 readonly DOCKER_COMPOSE_YML=$2
