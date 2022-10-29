faba96fd4fc70975249899a224300e84c9abf2de
yoshinobu-etou-id <yoshinobu_eto@intelligent-design.co.jp>
2022-10-29 16:16:45 +0900

    chore: カレントディレクトリ確認

--- backend/server_db_restore.sh
+++ backend/server_db_restore.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+echo $0
+DIR=$(cd $(dirname $0); pwd)
+echo $DIR
 source ./.env
 docker-compose exec -T db pg_restore --clean -h 127.0.0.1 -p 5432 -d $DB_NAME -U $DB_USER -v < /var/chiloportal.dump
--- backend/server_stop.sh
+++ backend/server_stop.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+echo $0
+DIR=$(cd $(dirname $0); pwd)
+echo $DIR
 readonly DOCKER_COMPOSE_YML=$2
 sudo cp $DOCKER_COMPOSE_YML docker-compose.yml
 /bin/sh ./server_db_backup.sh $ENV_FILE
