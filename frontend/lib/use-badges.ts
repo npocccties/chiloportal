6d6656052463f2bc642fc7f4c34388cf93d35189
Kimiaki Kuno <knokmki612@gmail.com>
2022-11-01 18:07:45 +0900

    feat: config.yaml と posts/*.md について Git 管理対象外にする

--- frontend/lib/use-badges.ts
+++ frontend/lib/use-badges.ts
@@ -7,13 +7,19 @@ const key = "badges";
 async function fetcher(
   _: typeof key,
   type: "wisdom" | "knowledge",
-  ids: number[]
+  badgesIds: number[]
 ) {
   return client.badges.$get({
-    query: { badges_type: type, badges_ids: ids.join(",") },
+    query: { badges_type: type, badges_ids: badgesIds.join(",") },
   });
 }
 
-export default function useBadges(type: "wisdom" | "knowledge", ids: number[]) {
-  return useSWRImmutable<BadgeDetail2[]>([key, type, ids], fetcher);
+export default function useBadges(
+  type: "wisdom" | "knowledge",
+  badgesIds: number[]
+) {
+  return useSWRImmutable<BadgeDetail2[]>(
+    badgesIds.length > 0 ? [key, type, badgesIds] : null,
+    fetcher
+  );
 }
