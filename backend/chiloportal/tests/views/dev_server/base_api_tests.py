fb8b21982eb8c3df390a1e7c24ae436122224f42
yoshinobu-etou-id <yoshinobu_eto@intelligent-design.co.jp>
2022-11-08 10:57:06 +0900

    chore: 実ドメインを example にする

--- backend/chiloportal/tests/views/dev_server/base_api_tests.py
+++ backend/chiloportal/tests/views/dev_server/base_api_tests.py
@@ -5,7 +5,8 @@ from rest_framework import status
 
 
 class BaseApiTests(TestCase):
-    base_url = "https://dev-portal.oku.cccties.org/api/v1/"
+    # 下記ドメインは実在するものに変更してからテストしてください
+    base_url = "https://dev-portal.oku.example.org/api/v1/"
     consumer_detail_url = urljoin(base_url, "consumer/")
     consumer_list_url = urljoin(base_url, "consumer/list/")
     stage_field_list_url = urljoin(base_url, "stage/field/list/")
