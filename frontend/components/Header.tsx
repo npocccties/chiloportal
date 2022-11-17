f2f4c721f030332fafdbb08c3cfd03bbf9a33fac
Kimiaki Kuno <knokmki612@gmail.com>
2022-11-17 15:22:49 +0900

    feat: ハンバーガーメニュー

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,14 +1,18 @@
+import { useId } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { Icon } from "@iconify/react";
 import Popover from "components/Popover";
 import SearchForm from "components/SearchForm";
 import useConsumers from "lib/use-consumers";
 import usePortalCategories from "lib/use-portal-categories";
+import useDialog from "lib/use-dialog";
 import clsx from "clsx";
 import { pagesPath } from "lib/$path";
 import contents from "lib/contents";
 import { NEXT_PUBLIC_MOODLE_DASHBOARD_URL } from "lib/env";
 import Fallback from "components/Fallback";
+import Menu from "components/Menu";
 
 type Props = {
   className?: string;
@@ -18,9 +22,26 @@ function Header({ className }: Props) {
   const { data: consumers, error: consumersError } = useConsumers();
   const { data: portalCategories, error: portalCategoriesError } =
     usePortalCategories();
+  const { open, onOpen, onClose } = useDialog();
+  const id = useId();
   return (
     <header className={clsx("bg-white", className)}>
       <div className="relative flex items-center gap-1 px-4 py-2 mx-auto max-w-6xl">
+        <button
+          className="jumpu-icon-button group md:hidden"
+          onClick={onOpen}
+          aria-describedby={id}
+        >
+          <Icon className="text-xl text-primary-500" icon="fa6-solid:bars" />
+          <span
+            id={id}
+            role="tooltip"
+            className="![transform:translate(-50%,_150%)_scale(0)] group-hover:![transform:translate(-50%,_150%)_scale(1)]"
+          >
+            メニュー
+          </span>
+        </button>
+        <Menu open={open} onClose={onClose} />
         <Link
           href={pagesPath.$url()}
           className="px-2 absolute md:static left-1/2 top-1/2 -translate-x-1/2 md:translate-x-0 -translate-y-1/2 md:translate-y-0 md:mr-4 shrink-0"
