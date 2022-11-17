16fab3b28c4faad2c10654637b686a7e79a123e9
Kimiaki Kuno <knokmki612@gmail.com>
2022-11-17 12:21:05 +0900

    feat: PortalCategoryCard の見た目の更新

--- frontend/components/PortalCategoryCard.tsx
+++ frontend/components/PortalCategoryCard.tsx
@@ -15,8 +15,14 @@ function PortalCategoryCard({ portalCategory, className }: Props) {
       href={pagesPath.portal_categories
         ._portalCategoryId(portalCategory.portal_category_id)
         .$url({ query: {} })}
-      className={clsx("block bg-white", className)}
+      className={clsx("flex flex-col bg-white", className)}
     >
+      <section className="px-4 pt-6 pb-4 flex-1">
+        <h3 className="flex justify-center items-center text-2xl text-gray-700 text-center font-bold mb-4">
+          {portalCategory.name}
+        </h3>
+        <p className="text-sm text-gray-500">{portalCategory.description}</p>
+      </section>
       <div className="relative">
         <Image
           className="block mx-auto pt-8 pb-4"
@@ -29,17 +35,9 @@ function PortalCategoryCard({ portalCategory, className }: Props) {
           {portalCategory.badges_count}
         </p>
       </div>
-      <section className="px-4 pt-2 pb-8">
-        <h3 className="flex justify-center items-center text-2xl text-gray-700 text-center mb-4">
-          {portalCategory.name}
-        </h3>
-        <p className="text-sm text-gray-500 mb-2">
-          {portalCategory.description}
-        </p>
-        <p className="text-xs text-gray-500">
-          能力バッジ（{portalCategory.badges_count}）
-        </p>
-      </section>
+      <p className="px-4 pt-2 pb-6 text-xs text-gray-500">
+        能力バッジ（{portalCategory.badges_count}）
+      </p>
     </Link>
   );
 }
