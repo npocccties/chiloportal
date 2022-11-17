300163504221db2f61dcc9e4b52543a257c1e6dd
Kimiaki Kuno <knokmki612@gmail.com>
2022-11-17 15:20:12 +0900

    refactor: 静的コンテンツの並びの外部化

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -6,43 +6,13 @@ import useConsumers from "lib/use-consumers";
 import usePortalCategories from "lib/use-portal-categories";
 import clsx from "clsx";
 import { pagesPath } from "lib/$path";
+import contents from "lib/contents";
 import { NEXT_PUBLIC_MOODLE_DASHBOARD_URL } from "lib/env";
 import Fallback from "components/Fallback";
 
 type Props = {
   className?: string;
 };
-
-const contents = [
-  {
-    title: "OKUTEPについて",
-    slug: "concept",
-  },
-  {
-    title: "私たちについて",
-    slug: "about_us",
-  },
-  {
-    title: "運営元について",
-    slug: "about_operator",
-  },
-  {
-    title: "プライバシーポリシー",
-    slug: "privacy_policy",
-  },
-  {
-    title: "ご利用にあたって（免責事項）",
-    slug: "disclaimer",
-  },
-  {
-    title: "教材の著作権について",
-    slug: "copyright",
-  },
-  {
-    title: "お問い合わせ",
-    slug: "contact",
-  },
-] as const;
 
 function Header({ className }: Props) {
   const { data: consumers, error: consumersError } = useConsumers();
---
+++ frontend/lib/contents.ts
@@ -0,0 +1,31 @@
+/** グローバルメニューにおける静的コンテンツの並び */
+export default [
+  {
+    title: "OKUTEPについて",
+    slug: "concept",
+  },
+  {
+    title: "私たちについて",
+    slug: "about_us",
+  },
+  {
+    title: "運営元について",
+    slug: "about_operator",
+  },
+  {
+    title: "プライバシーポリシー",
+    slug: "privacy_policy",
+  },
+  {
+    title: "ご利用にあたって（免責事項）",
+    slug: "disclaimer",
+  },
+  {
+    title: "教材の著作権について",
+    slug: "copyright",
+  },
+  {
+    title: "お問い合わせ",
+    slug: "contact",
+  },
+] as const;
