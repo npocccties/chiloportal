a1c231fc6c4bb28d86078a737370f598a274f22e
yoshinobu-etou-id <yoshinobu_eto@intelligent-design.co.jp>
2022-11-24 11:56:22 +0900

    chore: プロキシサーバーの接続先をCSVファイルに記載して読み込むようにした

--- backend/chiloportal/tests/views/dev_server/test_fuzzing_large.py
+++ backend/chiloportal/tests/views/dev_server/test_fuzzing_large.py
@@ -6,13 +6,18 @@ from .base_api_tests import *
 class FuzzingTests(BaseApiTests):
     @tag("fuzzing")
     def test_large_http_requests_200(self):
+        proxies = self.read_proxies()
         threads = []
-        for i in range(len(self.proxies)):
+        for i in range(len(proxies)):
             thread = threading.Thread(
                 target=self.request_frontend_framework_stages_proxy,
-                args=(self.proxies[i],),
+                args=(proxies[i],),
             )
             thread.start()
             threads.append(thread)
         for thread in threads:
             thread.join()
+        print(f"number of OK: {len(self.status_results_ok)}")
+        print(f"number of NG: {len(self.status_results_ng)}")
+        for ng in self.status_results_ng:
+            print(ng)
