9bf970552349845746688a5801aa93102e5bc3d3
Hidetaro7 <hidetaro7@gmail.com>
2022-12-29 17:28:14 +0900

    feat: 教育育成指標から探すトップの設計変更

--- frontend/templates/Frameworks.tsx
+++ frontend/templates/Frameworks.tsx
@@ -11,18 +11,18 @@ export default function Frameworks({
 }: Props) {
   const title = `${consumer.name}の教員育成指標`;
   return (
-    <Container as="article">
+    <Container as="article" className="mb-16">
       <Breadcrumbs
         className="mb-6"
         nodes={[{ name: "トップ", href: pagesPath.$url() }]}
         leaf={title}
       />
-      <h1 className="text-2xl mb-8">{title}</h1>
+      <h1 className="text-3xl md:text-4xl font-bold mb-8">{title}</h1>
       {frameworks.map((framework, frameworkIndex) => {
         const stages = stagesPerFrameworks[frameworkIndex];
         return (
-          <section key={framework.framework_id} className="mb-6">
-            <h2 className="text-lg mb-2">{framework.name}</h2>
+          <section key={framework.framework_id} className="mb-8 xl:mb-12">
+            <h2 className="text-xl font-bold mb-2">{framework.name}</h2>
             <StagesTable
               className="w-full max-w-[40rem]"
               consumer={consumer}
