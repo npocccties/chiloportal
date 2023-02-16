4065f0f0dd9edf0f5d71f6e4628cc81f2a23bebd
Hidetaro7 <hidetaro7@gmail.com>
2023-02-16 18:37:29 +0900

    feat: 下層ページのコンテンツがセンターに見えなかったので修正

--- frontend/templates/Content.tsx
+++ frontend/templates/Content.tsx
@@ -23,16 +23,16 @@ const components = {
 
 function Content({ source, matter }: Props) {
   return (
-    <Container>
+    <Container className="max-w-2xl mb-16">
       <Breadcrumbs
         className="mb-6"
         nodes={[{ name: "トップ", href: pagesPath.$url() }]}
         leaf={matter.title}
       />
-      <header className="prose mb-6">
+      <header className="prose max-w-none mb-6">
         <h1 className="mb-4">{matter.title}</h1>
       </header>
-      <article className="prose">
+      <article className="prose max-w-none">
         <MDXRemote {...source} components={components} />
       </article>
     </Container>
