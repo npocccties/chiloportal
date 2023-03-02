b9c7546a4bf71faf00360e1202414e46b15822b0
Hidetaro7 <hidetaro7@gmail.com>
2023-03-02 11:46:16 +0900

    fix: 教育育成指標のセクション文言確定

--- frontend/templates/Top.tsx
+++ frontend/templates/Top.tsx
@@ -290,18 +290,33 @@ export default function Top({
         </section>
         <section
           id="education-index"
-          className=" pb-16 relative before:bg-[url('/footer-bg.svg')] before:bg-no-repeat before:block
-          before:absolute before:-bottom-2 before:right-0 before:-z-1
-          before:w-[600px] before:h-[600px] before:bg-cover flex flex-col lg:flex-row border border-t-gray-200"
+          className="border-t border-gray-200 bg-gray-50"
         >
-          <div className="px-4 lg:px-16 pt-12 lg:pt-16 pb-8 relative z-10 ">
+          <div className="px-8 lg:px-16 py-12 lg:py-16 relative z-10 max-w-6xl mx-auto">
             <header className="mb-6">
               <h2 className="text-5xl notosansjp leading-tight font-bold mb-6 text-primary-700">
-                教員育成指標から探せる能力バッジ
+                地域課題とあなたのキャリアに応じて探せる能力バッジ
               </h2>
-              <p className="font-bold text-lg mb-4">
-                「教員育成指標」とは地域の教育委員会が教員に求められる資質や能力を、キャリア（経験年数）毎に明確にしたものです。
-              </p>
+              <section className="mb-8">
+                <h3 className="font-bold text-2xl mb-4 text-primary-700">
+                  地域ごとの課題を解決し、ニーズに応える
+                </h3>
+                <p className="leading-relaxed">
+                  各教育委員会では、地域の教育課題やニーズにあわせた教員の資質能力を教員育成指標として示しています。
+                  あなたの地域の教員育成指標から能力バッジを探すことで、あなたが直面している課題の解決、あるいは地域のニーズに応えられるかもしれません。
+                </p>
+              </section>
+              <section className="mb-8">
+                <h3 className="font-bold text-2xl mb-4 text-primary-700">
+                  キャリアにあわせた最適な学びへのアクセス
+                </h3>
+                <p className="leading-relaxed mb-4">
+                  教員育成指標は、教員のキャリアにあわせて身につけるべき能力をキャリアステージとして示しています。あなたが若手教員であってもベテラン教員であっても、教員育成指標から能力バッジを探すことで今のスキルや経験に応じて無理なく学ぶことができます。
+                </p>
+                <small className="text-gray-600 mb-4 block">
+                  「教員育成指標」とは地域の教育委員会が教員に求められる資質や能力を、キャリア（経験年数）毎に明確にしたものです。
+                </small>
+              </section>
               <Link
                 className="text-xs hover:underline whitespace-nowrap font-bold text-primary-700"
                 href={pagesPath._slug("about_badges").$url()}
@@ -354,15 +369,6 @@ export default function Top({
                 }
               </Fallback>
             </ul>
-          </div>
-          <div className="shrink-0 w-4/12 min-w-[300px] mx-auto lg:w-4/12 flex items-center justify-center">
-            <Image
-              src={"/bg-search-index.png"}
-              width={240}
-              height={240}
-              alt=""
-              className="w-full"
-            />
           </div>
         </section>
         <section id="other-contents" className="pb-16">
