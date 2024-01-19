b43f157daca41bd5923d13093487eb283771e69b
Kimiaki Kuno <knokmki612@gmail.com>
2024-01-20 02:27:27 +0900

    feat: 能力バッジを探す画面
    ルートと画面の実装までをスコープとしています。
    探すための機能については別途実装します。

---
+++ frontend/pages/discover.tsx
@@ -0,0 +1,28 @@
+import { GetServerSidePropsResult } from "next";
+import Error from "next/error";
+import Head from "next/head";
+import Template from "templates/Discover";
+import title from "lib/title";
+
+type ErrorProps = {
+  title: string;
+  statusCode: number;
+};
+
+export type Props = object;
+
+export async function getServerSideProps(): Promise<
+  GetServerSidePropsResult<ErrorProps | Props>
+> {
+  return { props: {} };
+}
+
+export default function Page(props: ErrorProps | Props) {
+  if ("statusCode" in props) return <Error {...props} />;
+  return (
+    <>
+      <Head>{title("バッジを探す")}</Head>
+      <Template {...props} />
+    </>
+  );
+}
---
+++ frontend/templates/Discover.tsx
@@ -0,0 +1,17 @@
+import Breadcrumbs from "components/Breadcrumbs";
+import Container from "components/Container";
+import { Props } from "pages/discover";
+import { pagesPath } from "lib/$path";
+
+export default function Discover({}: Props) {
+  return (
+    <Container as="article">
+      <Breadcrumbs
+        className="mb-6"
+        nodes={[{ name: "トップ", href: pagesPath.$url() }]}
+        leaf="バッジを探す"
+      />
+      <h1 className="text-3xl font-bold">バッジを探す</h1>
+    </Container>
+  );
+}
