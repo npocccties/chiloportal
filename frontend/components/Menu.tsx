80f1687c498ec41db7b520b5cf926296a662b313
Kimiaki Kuno <knokmki612@gmail.com>
2024-03-08 20:27:34 +0900

    fix: モバイルメニューが右からフレームインしたようにみえない
    DOMの生成とのタイミングか詳細は不明
    アニメーションする要素やCSSプロパティを調整して改善します

--- frontend/components/Menu.tsx
+++ frontend/components/Menu.tsx
@@ -30,123 +30,121 @@ function Menu({ open, onClose }: Props) {
           as={Fragment}
           enter="ease-out duration-300"
           enterFrom="bg-transparent"
-          enterTo="bg-[rgba(0,0,0,0.3)]"
+          enterTo="bg-black/30"
           leave="ease-out duration-300"
-          leaveFrom="bg-[rgba(0,0,0,0.3)]"
+          leaveFrom="bg-black/30"
           leaveTo="bg-transparent"
         >
+          <div className="fixed inset-0" />
+        </Transition.Child>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="left-full"
+          enterTo="left-0"
+          leave="ease-out duration-300"
+          leaveFrom="left-0"
+          leaveTo="left-full"
+        >
           <div className="fixed inset-0 overflow-y-auto">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="translate-x-3/4 opacity-0"
-              enterTo="translate-x-0 opacity-100"
-              leave="ease-out duration-300"
-              leaveFrom="translate-x-0 opacity-100"
-              leaveTo="translate-x-3/4 opacity-0"
-            >
-              <Dialog.Panel className="w-full bg-white px-4 pt-6 pb-12 min-h-full relative">
-                <div className="mb-8 sticky top-3 flex justify-end pr-6">
-                  <button
-                    className="text-right jumpu-icon-button group mb-8 sticky top-6"
-                    onClick={onClose}
-                    aria-describedby={id}
+            <Dialog.Panel className="w-screen bg-white px-4 pt-6 pb-12 min-h-full relative">
+              <div className="mb-8 sticky top-3 flex justify-end pr-6">
+                <button
+                  className="text-right jumpu-icon-button group mb-8 sticky top-6"
+                  onClick={onClose}
+                  aria-describedby={id}
+                >
+                  <Icon className="text-xl text-black" icon="fa6-solid:xmark" />
+                  <span
+                    id={id}
+                    role="tooltip"
+                    className="![transform:translate(-50%,_150%)_scale(0)] group-hover:![transform:translate(-50%,_150%)_scale(1)]"
                   >
-                    <Icon
-                      className="text-xl text-black"
-                      icon="fa6-solid:xmark"
-                    />
-                    <span
-                      id={id}
-                      role="tooltip"
-                      className="![transform:translate(-50%,_150%)_scale(0)] group-hover:![transform:translate(-50%,_150%)_scale(1)]"
+                    閉じる
+                  </span>
+                </button>
+              </div>
+              <ul className="mb-12 space-y-1">
+                <li>
+                  <Link
+                    className="jumpu-text-button font-bold w-full text-gray-700 hover:bg-gray-100"
+                    href={pagesPath.$url()}
+                  >
+                    ホーム
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="jumpu-text-button font-bold w-full text-gray-700 hover:bg-gray-100"
+                    href={pagesPath.discover.$url({ query: {} })}
+                  >
+                    学びを探す
+                  </Link>
+                </li>
+                <li>
+                  <p className="text-gray-400 font-bold px-rel5 py-rel3">
+                    大学トップ
+                  </p>
+                  <ul className="pl-4 mb-3 space-y-1">
+                    <Fallback
+                      data={issuers}
+                      error={issuersError}
+                      pending={[...Array(5)].map((_, index) => (
+                        <li
+                          key={index}
+                          className="animate-pulse rounded bg-gray-100 h-8 mb-1"
+                          aria-hidden
+                        ></li>
+                      ))}
                     >
-                      閉じる
-                    </span>
-                  </button>
-                </div>
-                <ul className="mb-12 space-y-1">
-                  <li>
+                      {(data) =>
+                        data.map((issuer) => (
+                          <li key={issuer.issuer_id} role="menuitem">
+                            <Link
+                              href={pagesPath.issuers
+                                ._issuerId(issuer.issuer_id)
+                                .$url()}
+                              className="jumpu-text-button font-bold w-full text-sm text-gray-700 hover:bg-gray-100"
+                            >
+                              {issuer.name}
+                            </Link>
+                          </li>
+                        ))
+                      }
+                    </Fallback>
+                  </ul>
+                </li>
+                <li>
+                  <Link
+                    className="jumpu-text-button font-bold w-full text-gray-700 hover:bg-gray-100"
+                    href={pagesPath.search.$url({ query: { q: "" } })}
+                  >
+                    キーワード検索
+                  </Link>
+                </li>
+                <li>
+                  <a
+                    className="jumpu-text-button font-bold w-full text-gray-700 hover:bg-gray-100"
+                    href={NEXT_PUBLIC_MOODLE_DASHBOARD_URL}
+                    rel="noopener noreferrer"
+                  >
+                    ログイン
+                  </a>
+                </li>
+              </ul>
+              <ul className="space-y-1">
+                {contents.map((content) => (
+                  <li key={content.slug}>
                     <Link
                       className="jumpu-text-button font-bold w-full text-gray-700 hover:bg-gray-100"
-                      href={pagesPath.$url()}
+                      href={pagesPath._slug(content.slug).$url()}
                     >
-                      ホーム
+                      {content.title}
                     </Link>
                   </li>
-                  <li>
-                    <Link
-                      className="jumpu-text-button font-bold w-full text-gray-700 hover:bg-gray-100"
-                      href={pagesPath.discover.$url({ query: {} })}
-                    >
-                      学びを探す
-                    </Link>
-                  </li>
-                  <li>
-                    <p className="text-gray-400 font-bold px-rel5 py-rel3">
-                      大学トップ
-                    </p>
-                    <ul className="pl-4 mb-3 space-y-1">
-                      <Fallback
-                        data={issuers}
-                        error={issuersError}
-                        pending={[...Array(5)].map((_, index) => (
-                          <li
-                            key={index}
-                            className="animate-pulse rounded bg-gray-100 h-8 mb-1"
-                            aria-hidden
-                          ></li>
-                        ))}
-                      >
-                        {(data) =>
-                          data.map((issuer) => (
-                            <li key={issuer.issuer_id} role="menuitem">
-                              <Link
-                                href={pagesPath.issuers
-                                  ._issuerId(issuer.issuer_id)
-                                  .$url()}
-                                className="jumpu-text-button font-bold w-full text-sm text-gray-700 hover:bg-gray-100"
-                              >
-                                {issuer.name}
-                              </Link>
-                            </li>
-                          ))
-                        }
-                      </Fallback>
-                    </ul>
-                  </li>
-                  <li>
-                    <Link
-                      className="jumpu-text-button font-bold w-full text-gray-700 hover:bg-gray-100"
-                      href={pagesPath.search.$url({ query: { q: "" } })}
-                    >
-                      キーワード検索
-                    </Link>
-                  </li>
-                  <li>
-                    <a
-                      className="jumpu-text-button font-bold w-full text-gray-700 hover:bg-gray-100"
-                      href={NEXT_PUBLIC_MOODLE_DASHBOARD_URL}
-                      rel="noopener noreferrer"
-                    >
-                      ログイン
-                    </a>
-                  </li>
-                </ul>
-                <ul className="space-y-1">
-                  {contents.map((content) => (
-                    <li key={content.slug}>
-                      <Link
-                        className="jumpu-text-button font-bold w-full text-gray-700 hover:bg-gray-100"
-                        href={pagesPath._slug(content.slug).$url()}
-                      >
-                        {content.title}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </Dialog.Panel>
-            </Transition.Child>
+                ))}
+              </ul>
+            </Dialog.Panel>
           </div>
         </Transition.Child>
       </Dialog>
