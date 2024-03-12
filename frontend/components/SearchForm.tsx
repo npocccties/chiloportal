6c56c93325b4c4b0f5fa2583d9f83cc5a0ec9323
Kimiaki Kuno <knokmki612@gmail.com>
2024-03-12 15:22:41 +0900

    feat: 検索アイコンは検索ボタンにする
    隣接してボタンが配置されている場合、検索欄のボタンに誤認されうる
    検索欄に検索ボタンを配置することで対処

--- frontend/components/SearchForm.tsx
+++ frontend/components/SearchForm.tsx
@@ -1,6 +1,6 @@
 import clsx from "clsx";
 import { Icon } from "@iconify/react";
-import { useState } from "react";
+import { useState, useId } from "react";
 import { useRouter } from "next/router";
 import { pagesPath } from "lib/$path";
 
@@ -20,6 +20,7 @@ function SearchForm({ className, size = "medium", variant = "dark" }: Props) {
     event.preventDefault();
     router.push(pagesPath.search.$url({ query: { q: keyword } }));
   };
+  const tooltipId = useId();
   return (
     <form
       className={clsx(
@@ -30,18 +31,10 @@ function SearchForm({ className, size = "medium", variant = "dark" }: Props) {
       )}
       onSubmit={handleSubmit}
     >
-      <Icon
-        className={clsx(
-          "absolute top-1/2 left-rel4 -translate-y-1/2",
-          variant === "dark" && "text-white",
-          variant === "light" && "text-dark",
-        )}
-        icon="fa-solid:search"
-      />
       <input
         required
         className={clsx(
-          "jumpu-input !rounded-full w-full !pl-rel10 pr-2",
+          "jumpu-input !rounded-full w-full !pr-rel11",
           variant === "dark" && "bg-black !text-white border-white",
         )}
         type="search"
@@ -49,6 +42,32 @@ function SearchForm({ className, size = "medium", variant = "dark" }: Props) {
         placeholder="学びたいキーワード"
         onInput={handleInput}
       />
+      <button
+        type="submit"
+        className={clsx(
+          "jumpu-icon-button rounded-full group absolute top-1/2 right-rel1 -translate-y-1/2",
+          size === "small" && "right-rel1",
+          size === "medium" && "right-rel2",
+          size === "large" && "right-rel3",
+          variant === "dark" && "hover:bg-gray-700",
+        )}
+        aria-describedby={tooltipId}
+      >
+        <Icon
+          className={clsx(
+            variant === "dark" && "text-white",
+            variant === "light" && "text-dark",
+          )}
+          icon="fa-solid:search"
+        />
+        <span
+          id={tooltipId}
+          role="tooltip"
+          className="-translate-x-1/2 !translate-y-[150%] group-hover:-translate-x-1/2 group-hover:translate-y-[150%]"
+        >
+          検索
+        </span>
+      </button>
     </form>
   );
 }
