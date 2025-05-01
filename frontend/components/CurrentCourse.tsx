01297157aff3c7f9cd1ffa15e923d2e26b62c0b2
Kimiaki Kuno <knokmki612@gmail.com>
2025-05-02 00:36:56 +0900

    feat: 受講中のコースと獲得したバッジの表示と一括操作

---
+++ frontend/components/CurrentCourse.tsx
@@ -0,0 +1,63 @@
+import DOMPurify from "isomorphic-dompurify";
+import { BadgeStatus } from "pages/dashboard";
+import { useRef } from "react";
+
+type Props = BadgeStatus & {
+  index: number;
+  onChecked?(index: number): void;
+  onUnchecked?(index: number): void;
+};
+
+function CurrentCourse(props: Props) {
+  const ref = useRef<HTMLInputElement>(null);
+  const handleClick = () => ref.current?.click();
+  return (
+    <div
+      className={
+        "jumpu-card pl-4 pr-6 py-4 flex items-center gap-4 has-checked:bg-primary-50"
+      }
+      onClick={handleClick}
+    >
+      <input
+        type="checkbox"
+        value={props.index}
+        className="jumpu-input disabled:bg-gray-200"
+        ref={ref}
+        onChange={(e) => {
+          if (e.currentTarget.checked) {
+            props.onChecked?.(props.index);
+          } else {
+            props.onUnchecked?.(props.index);
+          }
+        }}
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      />
+      <section>
+        <h3 className="text-lg font-semibold mb-2 truncate">
+          <a
+            className="hover:underline underline-offset-4 cursor-pointer"
+            href={
+              new URL(`/course/view.php?id=${props.course_id}`, props.lms_url)
+                .href
+            }
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+          >
+            {props.course_name}
+          </a>
+        </h3>
+        <div
+          className="prose text-sm max-w-none [&>p]:first:line-clamp-3 h-30"
+          dangerouslySetInnerHTML={{
+            __html: DOMPurify.sanitize(props.course_description ?? ""),
+          }}
+        />
+      </section>
+    </div>
+  );
+}
+
+export default CurrentCourse;
--- frontend/templates/Dashboard.tsx
+++ frontend/templates/Dashboard.tsx
@@ -1,10 +1,124 @@
-import { Props } from "pages/dashboard";
 import Breadcrumbs from "components/Breadcrumbs";
 import Container from "components/Container";
+import CurrentCourse from "components/CurrentCourse";
+import EarnedBadge from "components/EarnedBadge";
 import { pagesPath } from "lib/$path";
+import Link from "next/link";
+import { Props } from "pages/dashboard";
+import { useState } from "react";
+import {
+  NEXT_PUBLIC_BASE_URL,
+  NEXT_PUBLIC_CHILOWALLET_BASE_URL,
+} from "lib/env";
+
+function CurrentCourses(props: Pick<Props, "currentCourses">) {
+  const [selected, setSelected] = useState<Set<number>>(new Set());
+  const backUrl = new URL("/dashboard?tab=course", NEXT_PUBLIC_BASE_URL).href;
+  const importUrl = new URL(
+    `/badge/import?back_url=${encodeURIComponent(backUrl)}`,
+    NEXT_PUBLIC_CHILOWALLET_BASE_URL,
+  ).href;
+  const badgeStatusList = [];
+  for (const index of selected) {
+    const item = props.currentCourses[index];
+    if (!item) continue;
+    badgeStatusList.push(item);
+  }
+  return (
+    <form
+      className="space-y-2"
+      action={importUrl}
+      method="POST"
+      autoComplete="off"
+    >
+      {props.currentCourses.map((currentCourse, index) => (
+        <CurrentCourse
+          {...currentCourse}
+          key={index}
+          index={index}
+          onChecked={(index) => setSelected((prev) => new Set(prev.add(index)))}
+          onUnchecked={(index) =>
+            setSelected((prev) => {
+              const selected = new Set(prev);
+              selected.delete(index);
+              return selected;
+            })
+          }
+        />
+      ))}
+      <input
+        name="post_json"
+        type="hidden"
+        value={JSON.stringify({ badge_list: badgeStatusList })}
+      />
+      <div className="sticky bottom-0 p-2">
+        <button
+          className="jumpu-button text-lg font-bold w-full"
+          type="submit"
+          disabled={selected.size === 0}
+        >
+          {selected.size > 1 && `${selected.size} 件の`}バッジを獲得
+        </button>
+      </div>
+    </form>
+  );
+}
+
+function EarnedBadges(props: Pick<Props, "earnedBadges">) {
+  const [selected, setSelected] = useState<Set<number>>(new Set());
+  const backUrl = new URL("/dashboard?tab=course", NEXT_PUBLIC_BASE_URL).href;
+  const submitUrl = new URL(
+    `/badge/submission?back_url=${encodeURIComponent(backUrl)}`,
+    NEXT_PUBLIC_CHILOWALLET_BASE_URL,
+  ).href;
+  const badgeStatusList = [];
+  for (const index of selected) {
+    const item = props.earnedBadges[index];
+    if (!item) continue;
+    badgeStatusList.push(item);
+  }
+  return (
+    <form
+      className="space-y-2"
+      action={submitUrl}
+      method="POST"
+      autoComplete="off"
+    >
+      {props.earnedBadges.map((earnedBadge, index) => (
+        <EarnedBadge
+          {...earnedBadge}
+          key={index}
+          index={index}
+          onChecked={(index) => setSelected((prev) => new Set(prev.add(index)))}
+          onUnchecked={(index) =>
+            setSelected((prev) => {
+              const selected = new Set(prev);
+              selected.delete(index);
+              return selected;
+            })
+          }
+        />
+      ))}
+      <input
+        name="post_json"
+        type="hidden"
+        value={JSON.stringify({ badge_list: badgeStatusList })}
+      />
+      <div className="sticky bottom-0 p-2">
+        <button
+          className="jumpu-button text-lg font-bold w-full"
+          type="submit"
+          disabled={selected.size === 0}
+        >
+          {selected.size > 1 && `${selected.size} 件の`}バッジを提出
+        </button>
+      </div>
+    </form>
+  );
+}
 
 function Dashboard({
-  tab: _tab,
+  tab,
   currentCourses,
   earnedBadges,
   errorCode: _errorCode,
@@ -16,11 +130,40 @@ function Dashboard({
         nodes={[{ name: "トップ", href: pagesPath.$url() }]}
         leaf="ダッシュボード"
       />
-      <h1 className="text-3xl font-bold hidden md:block mb-8 border-b border-gray-300 pb-2">
+      <h1 className="text-3xl font-bold mb-8 border-b border-gray-300 pb-2">
         ダッシュボード
       </h1>
-      <pre>{JSON.stringify(currentCourses, null, 2)}</pre>
-      <pre>{JSON.stringify(earnedBadges, null, 2)}</pre>
+      <nav className="jumpu-boxed-tabs mb-4">
+        {/* TODO: jumpu-boxed-tabs が navigation role に対応したら追従して */}
+        <ul role="tablist">
+          <li role="tab" aria-selected={tab === "course"}>
+            <Link
+              href={pagesPath.dashboard.$url({
+                query: {
+                  tab: "course",
+                },
+              })}
+            >
+              受講中のコース
+            </Link>
+          </li>
+          <li role="tab" aria-selected={tab === "badge"}>
+            <Link
+              href={pagesPath.dashboard.$url({
+                query: {
+                  tab: "badge",
+                },
+              })}
+            >
+              獲得したバッジ
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <article className="h-[66svh] overflow-y-auto py-1">
+        {tab === "course" && <CurrentCourses currentCourses={currentCourses} />}
+        {tab === "badge" && <EarnedBadges earnedBadges={earnedBadges} />}
+      </article>
     </Container>
   );
 }
